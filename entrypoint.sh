c416a647b9279a59a0e0aaac2aa25796fceab1aa
Mac <mac@MacBook-Pro-de-Macbook.local>
2023-06-25 21:25:41 -0300

    feat: implementing routes and integration testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+# Wait for the DB to get up
 sleep 10
 
+# Install dependencies from requirements.txt
 pip install -r requirements.txt
+
+# Install python-creditcard library directly from Git repository
 pip install git+https://github.com/maistodos/python-creditcard.git@main
 
+# Apply database migrations
 echo "Apply database migrations"
 python manage.py makemigrations
 python manage.py migrate
@@ -20,5 +25,6 @@ else:
     print('Superuser created.')
 " | python manage.py shell
 
+# Start server
 echo "Starting server"
 exec "$@"
